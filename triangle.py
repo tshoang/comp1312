3e3e29f17ec1ae9e15aed4746a78fad8460d202c
Thái Sơn Hoàng <T.S.Hoang@soton.ac.uk>
2024-11-19 16:44:16 +0000

    Correct

--- triangle.py
+++ triangle.py
@@ -7,5 +7,19 @@ class Triangle(Enum):
     Equilateral = 'Equilateral'
 
 
-def triangle(a: int, b: int, c:int) -> Triangle:
-    pass
+def triangle(a: int, b: int, c: int) -> Triangle:
+    sorted_value = sorted([a, b, c])
+    if sorted_value[0] + sorted_value[1] <= sorted_value[2]:
+        raise ValueError("Not a proper triangle")
+
+    if a == b and b == c:
+        return Triangle.Equilateral
+
+    if a == b:
+        return Triangle.Isosceles
+    if b == c:
+        return Triangle.Isosceles
+    if c == a:
+        return Triangle.Isosceles
+
+    return Triangle.Scalene

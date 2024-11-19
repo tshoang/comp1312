1af9f8fc0b1d818f3c7a74f9e70734c24b554b67
Thái Sơn Hoàng <T.S.Hoang@soton.ac.uk>
2024-11-19 16:37:34 +0000

    Initial commit

---
+++ triangle.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+
+class Triangle(Enum):
+    Scalene = 'Scalene'
+    Isosceles = 'Isosceles'
+    Equilateral = 'Equilateral'
+
+
+def triangle(a: int, b: int, c:int) -> Triangle:
+    pass
